5a093793b3fd921304d3c30ee35799674389bf7c
mariiianimator <maryamalaei@yahoo.com>
2018-07-01 04:22:19 -0700

    initailized project

---
+++ cmake-build-debug/CMakeFiles/mipsproj.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mipsproj.dir/main.cpp.obj"
+  "mipsproj.pdb"
+  "mipsproj.exe"
+  "mipsproj.exe.manifest"
+  "libmipsproj.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mipsproj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
