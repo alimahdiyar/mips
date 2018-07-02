af5f5ea9c9845565177b3b1fdba78219c83f499d
mariiianimator <maryamalaei@yahoo.com>
2018-07-02 17:16:47 +0430

    iteration completed

--- main.cpp
+++ main.cpp
@@ -9,15 +9,15 @@
 
 using namespace std;
 
-class Register{
+class Register {
 public:
-    bool enable= true; //value jadid biyad dakhel reg ya na
+    bool enable = true; //value jadid biyad dakhel reg ya na
 
-    Register(){
+    Register() {
         Register::value = 0;
     }
 
-    Register(long value){
+    Register(long value) {
         Register::value = value;
     }
 
@@ -28,22 +28,25 @@ public:
     void setValue(long newValue) {
         Register::newValue = newValue;
     }
-    void tick(){
-        if(enable){
-            if(newValue != -1){
-                value=newValue;
-                newValue=-1;
-            } else{
-                enable=true;
+
+    void tick() {
+        if (enable) {
+            if (newValue != -1) {
+                value = newValue;
+                newValue = -1;
+            } else {
+                enable = true;
             }
         }
     }
+
 private:
     long value;// meghdar register
 protected:
-    long newValue=-1;//value jadid ke mikhad rekhte beshe dakhel reg
+    long newValue = -1;//value jadid ke mikhad rekhte beshe dakhel reg
 };
-class RegisterName{
+
+class RegisterName {
 public:
     enum Names {
         REG_0,
@@ -108,17 +111,37 @@ public:
 
 };
 
-class MemoryStore{
+class MemoryStore {
 private:
     vector<long> memory;
 public:
 
-    long getValue(long location){
+    MemoryStore(char filename[]) {
+        char f[4];
+        FILE *pFile;
+        pFile = fopen(filename, "r");
+        while (fscanf(pFile, "%c", &f[0]) != EOF) {
+            fscanf(pFile, "%c", &f[1]);
+            fscanf(pFile, "%c", &f[2]);
+            fscanf(pFile, "%c", &f[3]);
+
+            long word = getUnsignedValue(f[0]);
+            word += getUnsignedValue(f[1]) << 8;
+            word += getUnsignedValue(f[2]) << 16;
+            word += getUnsignedValue(f[3]) << 24;
+
+            // add the word to our memory store
+            memory.push_back(word);
+        }
+        fclose(pFile);
+    }
+
+    long getValue(long location) {
         long memAddress = location >> 2;
-        if(memAddress >= memory.size()){
+        if (memAddress >= memory.size()) {
             throw "Memory address out of bounds";
         }
-        return memory[(int)memAddress];
+        return memory[(int) memAddress];
     }
 
     void storeValue(long location, long value) {
@@ -127,8 +150,9 @@ public:
         if (memoryAddress >= memory.size()) {
             throw "Memory address out of bounds";
         }
-        memory[(int)memoryAddress] =  value;
+        memory[(int) memoryAddress] = value;
     }
+
     // age 1000 masalan bod adade manfi hast ba in mosbat mishe
     long getUnsignedValue(char b) {
         if (b >= 0) { return b; }
@@ -138,58 +162,75 @@ public:
 
 };
 
+
 class RegisterFile {
-protected:
-    map<RegisterName::Names , Register> registers;
-private:
-    bool enable = true;
-public:
- void setValue(RegisterName::Names registerName, long value) {
-        Register reg = getRegister(registerName);
-        reg.setValue(value);
-    }
+    protected:
+        map<RegisterName::Names, Register> registers;
+    private:
+        bool enable = true;
+    public:
+        void setValue(RegisterName::Names registerName, long value) {
+            Register reg = getRegister(registerName);
+            reg.setValue(value);
+        }
 
- long getValue(RegisterName::Names registerName) {
-        return getRegister(registerName).getValue();
-    }
+        long getValue(RegisterName::Names registerName) {
+            return getRegister(registerName).getValue();
+        }
 
- void disableWrite() {
-      enable = false;
-    }
+        void disableWrite() {
+            enable = false;
+        }
 
- void tick() {
-        if (enable) {
-            for (Register reg : registers.values()) {
-                reg.tick();
+        void tick() {
+            if (enable) {
+                for(map<RegisterName::Names , Register>::iterator k = registers.begin(); k != registers.end(); k++){
+                    (*k).second.tick();
+                }
+            } else {
+                enable = true;
             }
-        } else {
-            enable = true;
         }
-    }
 
 
-protected:
-    bool validRegister(RegisterName::Names registerName) {
-        return registerName.isPrimitive();
-    }
-
-private:
-    Register getRegister(RegisterName::Names registerName) {
-        if (!validRegister(registerName)) {
-            throw "Invalid register";
+    protected:
+        bool validRegister(RegisterName::Names registerName) {
+            //TODO: write isPrimitive()
+            //return registerName.isPrimitive();
         }
-        if (registers.find(registerName) == registers.end()) {
-            Register reg();
-            registers.insert(make_pair(registerName, reg));
+
+    private:
+        Register getRegister(RegisterName::Names registerName) {
+            if (!validRegister(registerName)) {
+                throw "Invalid register";
+            }
+            if (registers.find(registerName) == registers.end()) {
+                Register reg();
+                registers.insert(make_pair(registerName, reg));
+            }
+            return registers.at(registerName);
         }
-        return registers.at(registerName);
+    };
+
+
+long getUnsignedValue(char b) {
+    if (b >= 0) { return b; }
+
+    return 256 + b;
+}
+
+
+
+int main() {
+    //TODO: delete this
+    /*
+    map<string, string> x;
+    x.insert(make_pair("maryam", "sofi"));
+    x.insert(make_pair("ali", "camel"));
+    for(map<string, string>::iterator k = x.begin(); k != x.end(); k++){
+        cout << (*k).second << endl;
     }
-};
-
-
-
-int main()
-{
+    */
 
     return 0;
 }
